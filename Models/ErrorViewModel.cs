2191d9c13f04a055d9fd85648daf20ec6677f4e6
Moritz Schernus <moritz@schernus.org>
2022-03-30 10:36:40 +0200

    changed example project

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CSAProjektreview.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
