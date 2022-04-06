468d87b89818db8eba3ed7d568fb83677e906e8e
TheShelDark <sebastiangerz01@gmail.com>
2022-04-06 09:12:57 +0200

    added Azure SQL Database Connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,45 @@
+using System;
+using System.Data.SqlClient;
+using System.Text;
+
+try 
+    { 
+        SqlConnectionStringBuilder sql_builder = new SqlConnectionStringBuilder();
+
+        sql_builder.DataSource = "csa-app-server.database.windows.net"; 
+        sql_builder.UserID = "csa-admin";            
+        sql_builder.Password = "MininGerzSchernus2022!";     
+        sql_builder.InitialCatalog = "csa-app-db";
+    
+        using (SqlConnection connection = new SqlConnection(sql_builder.ConnectionString))
+        {
+            Console.WriteLine("\nQuery data example:");
+            Console.WriteLine("=========================================\n");
+            
+            connection.Open();       
+
+            String sql = "SELECT name, collation_name FROM sys.databases";
+
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                    }
+                }
+            }  
+                           
+        }
+    }
+    catch (SqlException e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+Console.WriteLine("\nDone.");
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +65,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+        
+    
